a3b48d6967e453547fa1e2e756eea7af7cb550a8
Jeremie Pelissier <jeremie@kintoun.fr>
2021-04-05 14:08:47 +0200

    Add comments + clean old codes

--- src/main.cpp
+++ src/main.cpp
@@ -25,6 +25,25 @@ TimerHandle_t bmegetDataTimer;
 
 Bsec bme_dev;
 
+/*******************************************************
+HELPERS FUNCTIONS
+********************************************************/
+
+/*!
+ * @brief Get the Mac address of the ESP network interface
+ *
+ * This function returns a human readable of the MAC address.
+ *
+ * @param
+ * @return [String] the MAC addresd
+ *
+ *
+  \code{.c}
+    // Example // 
+    MacasString();
+
+  \endcode
+ */
 String MACasString() {
   byte mac[6];
   WiFi.macAddress(mac);
@@ -36,16 +55,44 @@ String MACasString() {
   return ID;
 }
 
-void errLeds(void)
+/*!
+ * @brief dead end function when blocking error triggered.
+ *
+ * This function is to be called whenever an blocking error occurs (wiring, network,...)
+ * in order to let know an exterior intervention is required. It is an endless loop with blinking leds
+ *
+ * @param [in]      led_pin       the pin number where the led to be flashing. Default to 2 (ESP32)
+ * 
+ * @return None
+ * 
+ * \code{.c}
+ *  errLeds();
+ * \endcode
+ * 
+*/
+void errLeds(int led_pin = 2)
 {
-  pinMode(2, OUTPUT);
-  digitalWrite(2, HIGH);
+  pinMode(led_pin, OUTPUT);
+  digitalWrite(led_pin, HIGH);
   delay(100);
-  digitalWrite(2, LOW);
+  digitalWrite(led_pin, LOW);
   delay(100);
 }
 
-// Helper function definitions
+/*!
+ * @brief global checker function for the BME680 sensor.
+ *
+ * This function triggers the error handling function depending of the BSEC device status.
+ * See  type bsec_retrun_library_t in bsec_datatype.h for return codes.
+ *
+ * @param
+ * @return None
+ * 
+ * \code{.c}
+ *  checkBmeSensorStatus();
+ * \endcode
+ * 
+*/
 void checkBmeSensorStatus(void)
 {
   String output="";
@@ -79,6 +126,20 @@ void connectToMqtt() {
   amqttclient.connect();
 }
 
+/*!
+ * @brief Publish data from BME sensor over MQTT if ready
+ *
+ * This function creates a JSON Document to wrap the data collected from sensor (with run() function),
+ * and publish to MQTT broker
+ *
+ * @param
+ * @return None
+ * 
+ * \code{.c}
+ *  publishMQTT();
+ * \endcode
+ * 
+*/
 void publishMQTT() {
   if (bme_dev.run()) {
     StaticJsonDocument<128> doc;
@@ -101,6 +162,23 @@ void publishMQTT() {
   }
 }
 
+
+/*!
+ * @brief handler function for main WiFi events
+ *
+ * The function manages to reconnect WiFI if the status is disconnected,
+ * and try to connect to the MQTT broker if an IP address is got.
+ *
+ * @param [in] wifi_event         the WiFi event which triggered the handler. See WiFiType.h for status return list.
+ * @param [in] wifi_event_info    the info from the wifi event
+ * 
+ * @return None
+ * 
+ * \code{.c}
+ *  WiFi.onEvent(wifiEventHandler);
+ * \endcode
+ * 
+*/
 void wifiEventHandler(WiFiEvent_t wifi_event,WiFiEventInfo_t wifi_event_info) {
   switch (wifi_event) {
     case SYSTEM_EVENT_STA_CONNECTED:
@@ -127,12 +205,41 @@ void wifiEventHandler(WiFiEvent_t wifi_event,WiFiEventInfo_t wifi_event_info) {
   }
 }
 
+
+/*!
+ * @brief Callback function associated with MQTT connexion state.
+ *
+ * Do noting actually
+ *
+ * @param [out]    sessionPresent
+ * 
+ * @return None
+ * 
+ * \code{.c}
+ *  amqttclient.onConnect(onMqttConnect);
+ * \endcode
+ * 
+*/
 void onMqttConnect(bool sessionPresent) {
   Serial.println("Connected to MQTT");
   //uint16_t packetIdSub = amqttclient.subscribe("esp32/sub", 2);
   //uint16_t packetIdPub1 = amqttclient.publish("esp32/connection", 1, true, "connected");
 }
 
+/*!
+ * @brief Callback function associated with MQTT disconnection state.
+ *
+ * start a timer for MQTT connection
+ *
+ * @param [out]    reason      the reason why the MQTT connexion is disconnected
+ * 
+ * @return None
+ * 
+ * \code{.c}
+ *  amqttclient.onConnect(onMqttDisconnect);
+ * \endcode
+ * 
+*/
 void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
   Serial.println("Disconnected from MQTT");
   //Serial.println(String(millis())+" - Disconnected from MQTT broker : "+String(int8_t(reason)));
@@ -141,6 +248,21 @@ void onMqttDisconnect(AsyncMqttClientDisconnectReason reason) {
   }
 }
 
+/*!
+ * @brief Callback function associated with MQTT subscribe state.
+ *
+ * display acknowledged response.
+ *
+ * @param [out]     packetId      Id of the return packet
+ * @param [out]     qos           index of Quality of Service
+ * 
+ * @return None
+ * 
+ * \code{.c}
+ *  amqttclient.onConnect(onMqttSubscribe);
+ * \endcode
+ * 
+*/
 void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
   Serial.println("Subscribe acknowledged.");
   Serial.print("  packetId: ");
@@ -149,10 +271,44 @@ void onMqttSubscribe(uint16_t packetId, uint8_t qos) {
   //Serial.println(qos);
 }
 
+/*!
+ * @brief Callback function associated with MQTT incoming message state.
+ *
+ * deals with the message received from broker
+ *
+ * @param [out]     topic       topic the client subscribes to
+ * @param [out]     payload     the message content
+ * @param [out]     properties  
+ * @param [out]     len     
+ * @param [out]     index     
+ * @param [out]     total     
+ * 
+ * 
+ * @return None
+ * 
+ * \code{.c}
+ *  amqttclient.onConnect(onMqttMessage);
+ * \endcode
+ * 
+*/
 void onMqttMessage(char* topic, char* payload, AsyncMqttClientMessageProperties properties, size_t len, size_t index, size_t total){
   Serial.println("incoming message : "+String(topic)+" - "+payload);
 }
 
+/*!
+ * @brief Callback function associated with MQTT publish state.
+ *
+ * display acknowledged response after publishing a topic message
+ *
+ * @param [out]     packetId      Id of the return packet
+ * 
+ * @return None
+ * 
+ * \code{.c}
+ *  amqttclient.onConnect(onMqttPublish);
+ * \endcode
+ * 
+*/
 void onMqttPublish(uint16_t packetId) {
   Serial.println("Publish acknowledged.");
   Serial.print("  packetId: ");
@@ -163,6 +319,9 @@ void setup() {
   Serial.begin(115200);
   Serial.setDebugOutput(false);
 
+  //------------------------------------------
+  //WiFi and MQTT settings
+  //------------------------------------------
   mqttReconnectTimer = xTimerCreate("mqttTimer", pdMS_TO_TICKS(2000), pdFALSE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(connectToMqtt));
   
   WiFi.onEvent(wifiEventHandler);
@@ -176,29 +335,14 @@ void setup() {
   amqttclient.setKeepAlive(90);
 
   WiFi.begin(WIFI_SSID,WIFI_PASSWORD);
-  //mqttclient.onMessage(incomingMessageHandler);
-  //mqttclient.begin(BROKER_ADDR,BROKER_PORT,wificlient);
-  //mqttclient.setKeepAlive(3500);
 
-  // while (!mqttclient.connect("ESP32-192-168-1-36")) { //,BROKER_USER,BROKER_PWD)) {
-  //   Serial.print(".");
-  //   delay(100);
-  // }
-  // Serial.println("connected to MQTT broker");
-  
-  // if(!mqttclient.connected()){
-  //   Serial.println("Timeout!");
-  //   return;
-  // }
-
-  // mqttclient.subscribe("esp32/sub");
-  
+  //------------------------------------------
+  //BME680 settings
+  //------------------------------------------
   Wire.begin();
-  //bmegetDataTimer = xTimerCreate("bmeTimer", pdMS_TO_TICKS(6000), pdTRUE, (void*)0, reinterpret_cast<TimerCallbackFunction_t>(publishMQTT));
-  
   bme_dev.begin(BME680_I2C_ADDR_PRIMARY,Wire);
 
-  //bme_dev.setConfig(bsec_config_iaq);
+  bme_dev.setConfig(bsec_config_iaq);
 
   bsec_virtual_sensor_t sensorList[4] = {
     BSEC_OUTPUT_SENSOR_HEAT_COMPENSATED_HUMIDITY,
@@ -208,25 +352,11 @@ void setup() {
   };
   bme_dev.updateSubscription(sensorList,4,BSEC_SAMPLE_RATE_ULP);
   checkBmeSensorStatus();
-
-  // if (xTimerStart(bmegetDataTimer,0) != pdPASS) {
-  //   Serial.println("Erreur au démarrage du timer");
-  // }
 }
 
 void loop() {
 
-  // if(!mqttclient.connected()){
-  //   Serial.println(String(millis()));
-  //   Serial.println("Timeout Mqtt!");
-  //   while(true) {};
-  // }
-
-
-  // if (bme_dev.run()) {
      publishMQTT();
-  // } else {
-  //   checkBmeSensorStatus();
-  // }
+     
 }
 
